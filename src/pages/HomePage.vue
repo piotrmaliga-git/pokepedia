f8e787abee7c20d0824e15640fd5a7f4650bd3fa
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-08 18:57:16 +0200

    feat: add home page component with imported navbar and footer components

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <Navbar />
+  <Footer />
+</template>
+
+<script lang="ts">
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    Navbar: Navbar,
+    Footer: Footer,
+  },
+};
+</script>
