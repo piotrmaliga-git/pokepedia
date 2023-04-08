2cac127b9e9d769984f4067b732ae9cd79f4eef2
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-08 19:07:00 +0200

    conf: add main font Rubik into the tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('prettier-plugin-tailwindcss', 'eslint-plugin-tailwindcss', 'flowbite/plugin')],
 };
