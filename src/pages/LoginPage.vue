91eae2df4b43c6f3a2375514a22d8312c312fbe4
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-08 18:55:58 +0200

    feat: add log in page component with basic template from Flowbite

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,99 @@
+<template>
+  <section class="bg-gray-50 dark:bg-gray-900">
+    <div
+      class="mx-auto flex h-screen w-screen flex-col items-center justify-center px-6 py-8 lg:py-0">
+      <a
+        href="#"
+        class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
+        <img
+          class="mr-2 h-8 w-8"
+          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+          alt="logo" />
+        Flowbite
+      </a>
+      <div
+        class="w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0">
+        <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
+          <h1
+            class="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
+            Sign in to your account
+          </h1>
+          <form
+            class="space-y-4 md:space-y-6"
+            action="#">
+            <div>
+              <label
+                for="email"
+                class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                >Your email</label
+              >
+              <input
+                type="email"
+                name="email"
+                id="email"
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-blue-600 focus:ring-blue-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+                placeholder="name@company.com"
+                required="true" />
+            </div>
+            <div>
+              <label
+                for="password"
+                class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                >Password</label
+              >
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-blue-600 focus:ring-blue-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+                required="true" />
+            </div>
+            <div class="flex items-center justify-between">
+              <div class="flex items-start">
+                <div class="flex h-5 items-center">
+                  <input
+                    id="remember"
+                    aria-describedby="remember"
+                    type="checkbox"
+                    class="h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+                    required="true" />
+                </div>
+                <div class="ml-3 text-sm">
+                  <label
+                    for="remember"
+                    class="text-gray-500 dark:text-gray-300"
+                    >Remember me</label
+                  >
+                </div>
+              </div>
+              <a
+                href="#"
+                class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+                >Forgot password?</a
+              >
+            </div>
+            <button
+              type="submit"
+              class="w-full rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Sign in
+            </button>
+            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+              Don’t have an account yet?
+              <a
+                href="#"
+                class="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                >Sign up</a
+              >
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script lang="ts">
+export default {
+  name: 'LoginPage',
+};
+</script>
