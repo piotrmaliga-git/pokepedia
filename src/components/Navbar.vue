6c01071d5faa794f6bdbb34c846240a5fddf899e
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-10 14:09:14 +0200

    feat: restyle navbar from Flowbite

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,20 @@
 <template>
-  <div>
-    <nav class="border-gray-200 bg-white dark:bg-gray-900">
+  <div class="h-screen max-w-full bg-hero-image bg-cover bg-fixed bg-no-repeat">
+    <nav>
       <div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
         <a
-          href="https://flowbite.com/"
+          href="/"
           class="flex items-center">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="mr-3 h-8"
-            alt="Flowbite Logo" />
-          <span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white"
-            >Flowbite</span
-          >
+            src="../assets/images/logo/Pokepedia.png"
+            class="mr-3 h-12"
+            alt="Pokepedia Logo" />
         </a>
         <div class="flex items-center md:order-2">
           <button
             type="button"
             data-dropdown-toggle="language-dropdown-menu"
-            class="inline-flex cursor-pointer items-center justify-center rounded-lg px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white">
+            class="inline-flex cursor-pointer items-center justify-center rounded-lg px-4 py-2 text-sm font-medium text-white hover:bg-red-800">
             <svg
               class="mr-2 h-5 w-5 rounded-full"
               aria-hidden="true"
@@ -76,7 +73,7 @@
                   x="2470" />
               </g>
             </svg>
-            English (US)
+            English
           </button>
           <!-- Dropdown -->
           <div
@@ -118,7 +115,7 @@
                           transform="scale(3.9385)" />
                       </g>
                     </svg>
-                    English (US)
+                    English
                   </div>
                 </a>
               </li>
@@ -142,89 +139,7 @@
                         fill="#d00"
                         d="M0 170.7h512v170.6H0z" />
                     </svg>
-                    Deutsch
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                  role="menuitem">
-                  <div class="inline-flex items-center">
-                    <svg
-                      class="mr-2 h-3.5 w-3.5 rounded-full"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      id="flag-icon-css-it"
-                      viewBox="0 0 512 512">
-                      <g
-                        fill-rule="evenodd"
-                        stroke-width="1pt">
-                        <path
-                          fill="#fff"
-                          d="M0 0h512v512H0z" />
-                        <path
-                          fill="#009246"
-                          d="M0 0h170.7v512H0z" />
-                        <path
-                          fill="#ce2b37"
-                          d="M341.3 0H512v512H341.3z" />
-                      </g>
-                    </svg>
-                    Italiano
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                  role="menuitem">
-                  <div class="inline-flex items-center">
-                    <svg
-                      class="mr-2 h-3.5 w-3.5 rounded-full"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      xmlns:xlink="http://www.w3.org/1999/xlink"
-                      id="flag-icon-css-cn"
-                      viewBox="0 0 512 512">
-                      <defs>
-                        <path
-                          id="a"
-                          fill="#ffde00"
-                          d="M1-.3L-.7.8 0-1 .6.8-1-.3z" />
-                      </defs>
-                      <path
-                        fill="#de2910"
-                        d="M0 0h512v512H0z" />
-                      <use
-                        width="30"
-                        height="20"
-                        transform="matrix(76.8 0 0 76.8 128 128)"
-                        xlink:href="#a" />
-                      <use
-                        width="30"
-                        height="20"
-                        transform="rotate(-121 142.6 -47) scale(25.5827)"
-                        xlink:href="#a" />
-                      <use
-                        width="30"
-                        height="20"
-                        transform="rotate(-98.1 198 -82) scale(25.6)"
-                        xlink:href="#a" />
-                      <use
-                        width="30"
-                        height="20"
-                        transform="rotate(-74 272.4 -114) scale(25.6137)"
-                        xlink:href="#a" />
-                      <use
-                        width="30"
-                        height="20"
-                        transform="matrix(16 -19.968 19.968 16 256 230.4)"
-                        xlink:href="#a" />
-                    </svg>
-                    中文 (繁體)
+                    Polski
                   </div>
                 </a>
               </li>
@@ -254,11 +169,11 @@
           class="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
           id="mobile-menu-language-select">
           <ul
-            class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
+            class="mt-4 flex flex-col border p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0">
             <li>
               <a
                 href="#"
-                class="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+                class="block py-2 pl-3 pr-48 text-white hover:text-red-800 md:p-0"
                 aria-current="page"
                 >Home</a
               >
@@ -266,28 +181,28 @@
             <li>
               <a
                 href="#"
-                class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                class="block py-2 pl-3 pr-48 text-white hover:text-red-800 md:p-0"
                 >About</a
               >
             </li>
             <li>
               <a
                 href="#"
-                class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                class="block py-2 pl-3 pr-48 text-white hover:text-red-800 md:p-0"
                 >Services</a
               >
             </li>
             <li>
               <a
                 href="#"
-                class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                class="block py-2 pl-3 pr-48 text-white hover:text-red-800 md:p-0"
                 >Pricing</a
               >
             </li>
             <li>
               <a
                 href="#"
-                class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                class="block py-2 pl-3 pr-48 text-white hover:text-red-800 md:p-0"
                 >Contact</a
               >
             </li>
