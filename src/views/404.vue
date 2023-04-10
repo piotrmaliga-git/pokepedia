f5e89adc0c2bbb771484b0db5f5d035d044d8e8a
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-10 14:09:48 +0200

    feat: restyle 404 page from Flowbite

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,18 +1,16 @@
 <template>
-  <section class="bg-white dark:bg-gray-900">
+  <section class="bg-white">
     <div
       class="mx-auto flex h-screen w-screen max-w-screen-xl items-center justify-center py-8 px-4 lg:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-sm text-center">
-        <h1 class="mb-4 text-7xl font-extrabold tracking-tight text-white lg:text-9xl">404</h1>
-        <p class="mb-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white md:text-4xl">
-          Something's missing.
-        </p>
-        <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+        <h1 class="mb-4 text-7xl font-extrabold tracking-tight text-gray-300 lg:text-9xl">404</h1>
+        <p class="mb-4 text-3xl font-bold tracking-tight text-gray-900">Something's missing.</p>
+        <p class="mb-4 text-lg font-light text-gray-500">
           Sorry, we can't find that page. You'll find lots to explore on the home page.
         </p>
         <a
           href="/"
-          class="my-4 inline-flex rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+          class="my-4 inline-flex rounded-lg bg-red-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-900 focus:outline-none focus:ring-4 focus:ring-red-300"
           >Back to Homepage</a
         >
       </div>
