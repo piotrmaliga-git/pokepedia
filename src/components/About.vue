067bc6deca0e8f9d96e7f60f77ce462a534e2180
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-16 20:00:06 +0200

    feat: add About component into the Home view

---
+++ src/components/About.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="bg-gray-100">
+    <div
+      class="mx-auto max-w-screen-xl items-center gap-16 py-8 px-4 lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+      <div class="font-light text-gray-500 sm:text-lg">
+        <h2 class="mb-4 text-4xl font-extrabold tracking-tight text-gray-900">
+          We didn't reinvent the wheel
+        </h2>
+        <p class="mb-4">
+          We are strategists, designers and developers. Innovators and problem solvers. Small enough
+          to be simple and quick, but big enough to deliver the scope you want at the pace you need.
+          Small enough to be simple and quick, but big enough to deliver the scope you want at the
+          pace you need.
+        </p>
+        <p>
+          We are strategists, designers and developers. Innovators and problem solvers. Small enough
+          to be simple and quick.
+        </p>
+      </div>
+      <div class="mt-8 grid grid-cols-2 gap-4">
+        <img
+          class="w-full rounded-lg"
+          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+          alt="office content 1" />
+        <img
+          class="mt-4 w-full rounded-lg lg:mt-10"
+          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+          alt="office content 2" />
+      </div>
+    </div>
+    <div
+      class="mx-auto max-w-screen-xl items-center gap-16 py-8 px-4 lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+      <div class="mt-8 grid grid-cols-2 gap-4">
+        <img
+          class="mt-4 w-full rounded-lg lg:mt-10"
+          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+          alt="office content 1" />
+        <img
+          class="w-full rounded-lg"
+          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+          alt="office content 2" />
+      </div>
+      <div class="font-light text-gray-500 sm:text-lg">
+        <h2 class="mb-4 text-4xl font-extrabold tracking-tight text-gray-900">
+          We didn't reinvent the wheel
+        </h2>
+        <p class="mb-4">
+          We are strategists, designers and developers. Innovators and problem solvers. Small enough
+          to be simple and quick, but big enough to deliver the scope you want at the pace you need.
+          Small enough to be simple and quick, but big enough to deliver the scope you want at the
+          pace you need.
+        </p>
+        <p>
+          We are strategists, designers and developers. Innovators and problem solvers. Small enough
+          to be simple and quick.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+<script lang="ts">
+export default {
+  name: 'About',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <Navbar />
+  <About/>
   <Footer />
 </template>
 
 <script lang="ts">
 import Navbar from '@/components/Navbar.vue';
+import About from '@/components/About.vue';
 import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'Home',
   components: {
     Navbar: Navbar,
+    About: About,
     Footer: Footer,
   },
 };
