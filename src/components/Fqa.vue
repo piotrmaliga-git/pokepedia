79346c8d70d612f232bc8ead3928cf91e09119a9
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-17 21:39:28 +0200

    style: change svg icon to icon from iconify

--- src/components/Fqa.vue
+++ src/components/Fqa.vue
@@ -15,17 +15,10 @@
             aria-expanded="true"
             aria-controls="accordion-collapse-body-1">
             <span>Eos facere omnis magni doloremque, alias repellat quaerat ?</span>
-            <svg
-              data-accordion-icon
-              class="h-6 w-6 shrink-0 rotate-180"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"></path>
-            </svg>
+            <Icon
+              icon="mdi:chevron-down"
+              width="24"
+              height="24" />
           </button>
         </h2>
         <div
@@ -58,17 +51,10 @@
             aria-expanded="false"
             aria-controls="accordion-collapse-body-2">
             <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit ?</span>
-            <svg
-              data-accordion-icon
-              class="h-6 w-6 shrink-0"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"></path>
-            </svg>
+            <Icon
+              icon="mdi:chevron-down"
+              width="24"
+              height="24" />
           </button>
         </h2>
         <div
@@ -102,17 +88,10 @@
             aria-expanded="false"
             aria-controls="accordion-collapse-body-3">
             <span>Labore tempora, perspiciatis fuga ?</span>
-            <svg
-              data-accordion-icon
-              class="h-6 w-6 shrink-0"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"></path>
-            </svg>
+            <Icon
+              icon="mdi:chevron-down"
+              width="24"
+              height="24" />
           </button>
         </h2>
         <div
@@ -158,7 +137,12 @@
 </template>
 
 <script lang="ts">
+import { Icon } from '@iconify/vue';
+
 export default {
   name: 'Fqa',
+  components: {
+    Icon,
+  },
 };
 </script>
