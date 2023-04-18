00fafc4115eab67cf079612d258ae5d2aa862871
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-18 21:13:49 +0200

    style: change default svg icons to the icons from Iconify

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,69 +16,17 @@
               type="button"
               data-dropdown-toggle="language-dropdown-menu"
               class="inline-flex cursor-pointer items-center justify-center rounded-lg px-4 py-2 text-sm font-medium text-white hover:bg-red-600">
-              <svg
-                class="mr-2 h-5 w-5 rounded-full"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                viewBox="0 0 3900 3900">
-                <path
-                  fill="#b22234"
-                  d="M0 0h7410v3900H0z" />
-                <path
-                  d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
-                  stroke="#fff"
-                  stroke-width="300" />
-                <path
-                  fill="#3c3b6e"
-                  d="M0 0h2964v2100H0z" />
-                <g fill="#fff">
-                  <g id="d">
-                    <g id="c">
-                      <g id="e">
-                        <g id="b">
-                          <path
-                            id="a"
-                            d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z" />
-                          <use
-                            xlink:href="#a"
-                            y="420" />
-                          <use
-                            xlink:href="#a"
-                            y="840" />
-                          <use
-                            xlink:href="#a"
-                            y="1260" />
-                        </g>
-                        <use
-                          xlink:href="#a"
-                          y="1680" />
-                      </g>
-                      <use
-                        xlink:href="#b"
-                        x="247"
-                        y="210" />
-                    </g>
-                    <use
-                      xlink:href="#c"
-                      x="494" />
-                  </g>
-                  <use
-                    xlink:href="#d"
-                    x="988" />
-                  <use
-                    xlink:href="#c"
-                    x="1976" />
-                  <use
-                    xlink:href="#e"
-                    x="2470" />
-                </g>
-              </svg>
+              <Icon
+                icon="emojione:flag-for-united-states"
+                width="22"
+                height="22"
+                class="mr-2"
+                aria-hidden="true" />
               English
             </button>
             <!-- Dropdown -->
             <div
-              class="z-50 my-4 hidden list-none divide-y divide-gray-100 rounded-lg bg-white text-base shadow"
+              class="z-50 my-4 hidden list-none divide-y divide-gray-100 rounded-lg bg-gray-100 text-base shadow"
               id="language-dropdown-menu">
               <ul
                 class="py-2 font-medium"
@@ -86,36 +34,15 @@
                 <li>
                   <a
                     href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200"
                     role="menuitem">
                     <div class="inline-flex items-center">
-                      <svg
-                        aria-hidden="true"
-                        class="mr-2 h-3.5 w-3.5 rounded-full"
-                        xmlns="http://www.w3.org/2000/svg"
-                        id="flag-icon-css-us"
-                        viewBox="0 0 512 512">
-                        <g fill-rule="evenodd">
-                          <g stroke-width="1pt">
-                            <path
-                              fill="#bd3d44"
-                              d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                              transform="scale(3.9385)" />
-                            <path
-                              fill="#fff"
-                              d="M0 10h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                              transform="scale(3.9385)" />
-                          </g>
-                          <path
-                            fill="#192f5d"
-                            d="M0 0h98.8v70H0z"
-                            transform="scale(3.9385)" />
-                          <path
-                            fill="#fff"
-                            d="M8.2 3l1 2.8H12L9.7 7.5l.9 2.7-2.4-1.7L6 10.2l.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7L74 8.5l-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 7.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 24.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 21.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 38.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 35.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 52.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 49.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 66.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 63.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9z"
-                            transform="scale(3.9385)" />
-                        </g>
-                      </svg>
+                      <Icon
+                        icon="emojione:flag-for-united-states"
+                        width="16"
+                        height="16"
+                        class="mr-2"
+                        aria-hidden="true" />
                       English
                     </div>
                   </a>
@@ -123,23 +50,14 @@
                 <li>
                   <a
                     href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200"
                     role="menuitem">
                     <div class="inline-flex items-center">
-                      <svg
-                        class="mr-2 h-3.5 w-3.5 rounded-full"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        id="flag-icon-css-de"
-                        viewBox="0 0 512 512">
-                        <path
-                          fill="#ffce00"
-                          d="M0 341.3h512V512H0z" />
-                        <path d="M0 0h512v170.7H0z" />
-                        <path
-                          fill="#d00"
-                          d="M0 170.7h512v170.6H0z" />
-                      </svg>
+                      <Icon
+                        icon="emojione:flag-for-poland"
+                        width="16"
+                        height="16"
+                        class="mr-2" />
                       Polski
                     </div>
                   </a>
@@ -153,17 +71,11 @@
               aria-controls="mobile-menu-language-select"
               aria-expanded="false">
               <span class="sr-only">Open main menu</span>
-              <svg
-                class="h-6 w-6"
-                fill="currentColor"
-                aria-hidden="true"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"></path>
-              </svg>
+              <Icon
+                icon="charm:menu-hamburger"
+                width="24"
+                height="24"
+                aria-hidden="true" />
             </button>
           </div>
           <div
@@ -233,10 +145,13 @@
 <script lang="ts">
 import Hero from './Hero.vue';
 
+import { Icon } from '@iconify/vue';
+
 export default {
   name: 'Navbar',
   components: {
     Hero: Hero,
+    Icon,
   },
 };
 </script>
