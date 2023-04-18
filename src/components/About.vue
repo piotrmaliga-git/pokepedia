6f744988ec6c5727b02777992ba05e3189fd837d
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-18 22:26:52 +0200

    style: add styling change sin the About Component

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,17 +4,19 @@
       class="mx-auto max-w-screen-xl items-center gap-16 py-8 px-4 lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
       <div class="font-light text-gray-500 sm:text-lg">
         <h2 class="mb-4 text-4xl font-extrabold tracking-tight text-gray-900">
-          We didn't reinvent the wheel
+          Witae itaque excepturi dignissimos quidem esse
         </h2>
         <p class="mb-4">
-          We are strategists, designers and developers. Innovators and problem solvers. Small enough
-          to be simple and quick, but big enough to deliver the scope you want at the pace you need.
-          Small enough to be simple and quick, but big enough to deliver the scope you want at the
-          pace you need.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam eos hic exercitationem iste
+          molestias expedita, consequatur blanditiis repellat esse illo pariatur voluptatibus
+          ducimus harum tempore ullam quos, autem laborum nostrum. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Nulla velit culpa voluptatem placeat voluptatibus, ducimus
+          laboriosam molestiae impedit excepturi omnis quo delectus architecto assumenda voluptates
+          veniam quod nihil dolorum dolorem.
         </p>
         <p>
-          We are strategists, designers and developers. Innovators and problem solvers. Small enough
-          to be simple and quick.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus recusandae ipsum
+          tenetur culpa consequuntur fugiat vel, iste explicabo.
         </p>
       </div>
       <div class="mt-8 grid grid-cols-2 gap-4">
@@ -29,30 +31,33 @@
       </div>
     </div>
     <div
-      class="mx-auto max-w-screen-xl items-center gap-16 py-8 px-4 lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+      class="mx-auto max-w-screen-xl items-center gap-16 py-8 px-4 sm:py-2 lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
       <div class="mt-8 grid grid-cols-2 gap-4">
         <img
           class="mt-4 w-full rounded-lg lg:mt-10"
-          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
-          alt="office content 1" />
-        <img
-          class="w-full rounded-lg"
           src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
           alt="office content 2" />
+        <img
+          class="w-full rounded-lg"
+          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+          alt="office content 1" />
       </div>
-      <div class="font-light text-gray-500 sm:text-lg">
+
+      <div class="mt-8 font-light text-gray-500 sm:text-lg">
         <h2 class="mb-4 text-4xl font-extrabold tracking-tight text-gray-900">
-          We didn't reinvent the wheel
+          Lorem ipsum dolor sit amet consectetur?
         </h2>
         <p class="mb-4">
-          We are strategists, designers and developers. Innovators and problem solvers. Small enough
-          to be simple and quick, but big enough to deliver the scope you want at the pace you need.
-          Small enough to be simple and quick, but big enough to deliver the scope you want at the
-          pace you need.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, facilis ullam
+          blanditiis dolorum explicabo corrupti, accusamus tempore harum deserunt eligendi, fugiat
+          necessitatibus quos voluptas beatae a rem error? Dolor, doloremque!
         </p>
         <p>
-          We are strategists, designers and developers. Innovators and problem solvers. Small enough
-          to be simple and quick.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus pariatur ipsa iusto, ut
+          omnis repellat dignissimos fugiat cupiditate expedita. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Earum cum beatae praesentium libero impedit molestias
+          tempora aut omnis consequatur facilis, incidunt consequuntur dicta, numquam nulla eius
+          iure sit assumenda obcaecati.
         </p>
       </div>
     </div>
