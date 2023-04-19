49cc99349e20c106274640d340b551f7ad0b1244
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-19 23:31:02 +0200

    style: implement styling fixes in the 404 component

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bg-white">
+  <section class="bg-gray-100">
     <div
       class="mx-auto flex h-screen w-screen max-w-screen-xl items-center justify-center py-8 px-4 lg:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-sm text-center">
@@ -10,7 +10,7 @@
         </p>
         <a
           href="/"
-          class="my-4 inline-flex rounded-lg bg-red-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-900 focus:outline-none focus:ring-4 focus:ring-red-300"
+          class="my-4 inline-flex rounded-lg bg-red-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:scale-105 hover:bg-red-800"
           >Back to Homepage</a
         >
       </div>
