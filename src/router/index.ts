a6058b6d290eb4053d6816fb003cb53e3c83cfe4
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-23 11:16:52 +0200

    feat: add custom page tab tittle depends on routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,18 +7,25 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('@/views/Home.vue'),
+      meta: { title: 'Poképedia | Home' },
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('@/views/Login.vue'),
+      meta: { title: 'Poképedia | Login' },
     },
     {
       path: '/:pathMatch(.*)*',
       name: '404',
       component: () => import('@/views/404.vue'),
+      meta: { title: 'Poképedia | Not Found' },
     },
   ],
+});
+
+router.beforeEach(to => {
+  document.title = to.meta?.title ?? 'Poképedia';
 });
 
 export default router;
