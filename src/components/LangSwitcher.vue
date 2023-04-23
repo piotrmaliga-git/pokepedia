231d9f86b88b6e7b0a3b3dd48569afb5d510c897
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-23 21:08:30 +0200

    ref: split Navbar Component into smaller pieces. Add LangSwitcher Component

---
+++ src/components/LangSwitcher.vue
@@ -0,0 +1,58 @@
+<template>
+  <button
+    type="button"
+    data-dropdown-toggle="language-dropdown-menu"
+    class="inline-flex cursor-pointer items-center justify-center rounded-lg px-4 py-2 text-sm font-medium text-white hover:bg-red-600">
+    <Icon
+      icon="emojione:flag-for-united-states"
+      width="22"
+      height="22"
+      class="mr-2"
+      aria-hidden="true" />
+    English
+  </button>
+  <!-- Dropdown -->
+  <div
+    class="z-50 my-4 hidden list-none divide-y divide-gray-100 rounded-lg bg-gray-100 text-base shadow"
+    id="language-dropdown-menu">
+    <ul
+      class="py-2 font-medium"
+      role="none">
+      <li>
+        <a
+          href="#"
+          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200"
+          role="menuitem">
+          <div class="inline-flex items-center">
+            <Icon
+              icon="emojione:flag-for-united-states"
+              width="16"
+              height="16"
+              class="mr-2"
+              aria-hidden="true" />
+            English
+          </div>
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200"
+          role="menuitem">
+          <div class="inline-flex items-center">
+            <Icon
+              icon="emojione:flag-for-poland"
+              width="16"
+              height="16"
+              class="mr-2" />
+            Polski
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue';
+</script>
