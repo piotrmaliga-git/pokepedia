1a4bd31e7507e68d00b09ddc290b7d171f1533d2
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-23 21:13:36 +0200

    ref: split Navbar Component into smaller pieces. Add NavbarLinks Component

---
+++ src/components/NavbarLinks.vue
@@ -0,0 +1,61 @@
+<template>
+  <div
+    class="hidden w-full items-center justify-between lg:order-1 lg:flex lg:w-auto"
+    id="mobile-menu-language-select">
+    <ul
+      class="mt-4 flex flex-col rounded-lg border bg-gray-100 p-4 font-medium lg:mt-0 lg:flex-row lg:space-x-8 lg:border-0 lg:bg-transparent lg:p-0">
+      <li>
+        <a
+          href="#"
+          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          aria-current="page"
+          >Home</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          >About</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          >Pokédex</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          >Games & Apps</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          >Trading Card Game</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          >Contact</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          >FQA</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts"></script>
