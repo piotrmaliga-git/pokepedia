649aa06070959c2a8310abec426eae942d1b46d8
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-04-30 13:22:07 +0200

    style: add dark mode colors using tailwind class in the NavbarLinks component

--- src/components/NavbarLinks.vue
+++ src/components/NavbarLinks.vue
@@ -3,11 +3,11 @@
     class="hidden w-full items-center justify-between lg:order-1 lg:flex lg:w-auto"
     id="mobile-menu-language-select">
     <ul
-      class="mt-4 flex flex-col rounded-lg border bg-gray-100 p-4 font-medium lg:mt-0 lg:flex-row lg:space-x-8 lg:border-0 lg:bg-transparent lg:p-0">
+      class="mt-4 flex flex-col rounded-lg bg-gray-100 p-4 font-medium dark:bg-gray-700 lg:mt-0 lg:flex-row lg:space-x-8 lg:border-0 lg:bg-transparent lg:p-0 dark:lg:bg-transparent">
       <li>
         <a
           href="#"
-          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
           aria-current="page"
           >Home</a
         >
@@ -15,42 +15,42 @@
       <li>
         <a
           href="#"
-          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
           >About</a
         >
       </li>
       <li>
         <a
           href="#"
-          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
           >Pokédex</a
         >
       </li>
       <li>
         <a
           href="#"
-          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
           >Games & Apps</a
         >
       </li>
       <li>
         <a
           href="#"
-          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
           >Trading Card Game</a
         >
       </li>
       <li>
         <a
           href="#"
-          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
           >Contact</a
         >
       </li>
       <li>
         <a
           href="#"
-          class="block py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600"
+          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
           >FAQ</a
         >
       </li>
