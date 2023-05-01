7b87c65d0ddfecc404f1c074f17fa2eb908e3ed7
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-01 19:03:07 +0200

    fix: remove unnecessary div from Footer component template

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,59 +1,57 @@
 <template>
-  <div>
-    <footer class="bg-red-600">
-      <div class="mx-auto w-full max-w-screen-xl p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="/"
-            class="mb-4 flex items-center sm:mb-0">
-            <img
-              src="../assets/images/logo/Pokepedia.png"
-              class="mr-3 h-12"
-              alt="Pokepedia Logo" />
-          </a>
-          <ul class="mb-6 flex flex-wrap items-center text-sm font-medium text-white sm:mb-0">
-            <li>
-              <a
-                href="#"
-                class="mr-4 hover:underline md:mr-6"
-                >About</a
-              >
-            </li>
-            <li>
-              <a
-                href="#"
-                class="mr-4 hover:underline md:mr-6"
-                >Privacy Policy</a
-              >
-            </li>
-            <li>
-              <a
-                href="#"
-                class="mr-4 hover:underline md:mr-6"
-                >Licensing</a
-              >
-            </li>
-            <li>
-              <a
-                href="#"
-                class="hover:underline"
-                >Contact</a
-              >
-            </li>
-          </ul>
-        </div>
-        <hr class="my-6 border-l-gray-200 sm:mx-auto lg:my-8" />
-        <span class="block text-sm text-white sm:text-center"
-          >&copy; {{ displayCurrentYear() }}
-          <a
-            href="/"
-            class="hover:underline"
-            >Pokepedia&trade;</a
-          >. All Rights Reserved.</span
-        >
+  <footer class="bg-red-600">
+    <div class="mx-auto w-full max-w-screen-xl p-4 md:py-8">
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <a
+          href="/"
+          class="mb-4 flex items-center sm:mb-0">
+          <img
+            src="../assets/images/logo/Pokepedia.png"
+            class="mr-3 h-12"
+            alt="Pokepedia Logo" />
+        </a>
+        <ul class="mb-6 flex flex-wrap items-center text-sm font-medium text-white sm:mb-0">
+          <li>
+            <a
+              href="#"
+              class="mr-4 hover:underline md:mr-6"
+              >About</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="mr-4 hover:underline md:mr-6"
+              >Privacy Policy</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="mr-4 hover:underline md:mr-6"
+              >Licensing</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="hover:underline"
+              >Contact</a
+            >
+          </li>
+        </ul>
       </div>
-    </footer>
-  </div>
+      <hr class="my-6 border-l-gray-200 sm:mx-auto lg:my-8" />
+      <span class="block text-sm text-white sm:text-center"
+        >&copy; {{ displayCurrentYear() }}
+        <a
+          href="/"
+          class="hover:underline"
+          >Pokepedia&trade;</a
+        >. All Rights Reserved.</span
+      >
+    </div>
+  </footer>
 </template>
 
 <script lang="ts" setup>
