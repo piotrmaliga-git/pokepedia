a231c3cd8225deaaa78f9bc0b46ca574f525ee8c
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-07 19:11:54 +0200

    feat: mock 12 pokemon to style grid of cards

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,76 +1,153 @@
 <template>
-  <div class="grid grid-cols-2 gap-4 md:grid-cols-3">
-    <div>
+  <div class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/1.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Bulbasaur</h2>
+      <p class="py-2 text-black">#0001</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#9BCC50] py-1 px-2 text-gray-600">Grass</span>
+        <span class="rounded-md bg-[#B97FC9] py-1 px-2">Poison</span>
+      </p>
     </div>
-    <div>
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/2.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Ivysaur</h2>
+      <p class="py-2 text-black">#0002</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#9BCC50] py-1 px-2 text-gray-600">Grass</span>
+        <span class="rounded-md bg-[#B97FC9] py-1 px-2">Poison</span>
+      </p>
     </div>
-    <div>
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/3.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Venusaur</h2>
+      <p class="py-2 text-black">#0003</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#9BCC50] py-1 px-2 text-gray-600">Grass</span>
+        <span class="rounded-md bg-[#B97FC9] py-1 px-2">Poison</span>
+      </p>
     </div>
-    <div>
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/4.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Charmander</h2>
+      <p class="py-2 text-black">#0004</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#FD7D24] py-1 px-2">Fire</span>
+      </p>
     </div>
-    <div>
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/5.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Charmeleon</h2>
+      <p class="py-2 text-black">#0005</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#FD7D24] py-1 px-2">Fire</span>
+      </p>
     </div>
-    <div>
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/6.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Charizard</h2>
+      <p class="py-2 text-black">#0006</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#FD7D24] py-1 px-2">Fire</span>
+        <span class="rounded-md bg-[#BDB9B8] py-1 px-2">Flying</span>
+      </p>
     </div>
-    <div>
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/7.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Squirtle</h2>
+      <p class="py-2 text-center text-black">#0007</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#4592C4] py-1 px-2">Water</span>
+      </p>
     </div>
-    <div>
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/8.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Wartortle</h2>
+      <p class="py-2 text-center text-black">#0008</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#4592C4] py-1 px-2">Water</span>
+      </p>
     </div>
-    <div>
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/9.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Blastoise</h2>
+      <p class="py-2 text-center text-black">#0009</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#4592C4] py-1 px-2">Water</span>
+      </p>
     </div>
-    <div>
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/10.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Caterpie</h2>
+      <p class="py-2 text-center text-black">#0010</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#729F3F] py-1 px-2">Bug</span>
+      </p>
     </div>
-    <div>
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/11.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Metapod</h2>
+      <p class="py-2 text-center text-black">#0011</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#729F3F] py-1 px-2">Bug</span>
+      </p>
     </div>
-    <div>
+    <div
+      class="flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-200 p-10">
       <img
-        class="h-auto max-w-full rounded-lg"
-        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+        class="mb-2 h-40"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/12.svg"
         alt="" />
+      <h2 class="text-3xl font-normal capitalize">Butterfree</h2>
+      <p class="py-2 text-center text-black">#0012</p>
+      <p class="text-sm text-white">
+        <span class="mr-2 rounded-md bg-[#729F3F] py-1 px-2">Bug</span>
+        <span class="rounded-md bg-[#BDB9B8] py-1 px-2">Flying</span>
+      </p>
     </div>
   </div>
 </template>
