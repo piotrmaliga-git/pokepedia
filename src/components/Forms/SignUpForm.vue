77ab9a3b1052c3607397052b9ecfe04ac905d322
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-13 23:07:39 +0200

    feat: add Sign up form with basic template from Flowbite

---
+++ src/components/Forms/SignUpForm.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
+    <h1
+      class="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
+      Create and account
+    </h1>
+    <form
+      class="space-y-4 md:space-y-6"
+      action="#">
+      <div>
+        <label
+          for="email"
+          class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+          >Your email</label
+        >
+        <input
+          type="email"
+          name="email"
+          id="email"
+          class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+          placeholder="name@company.com"
+          required="" />
+      </div>
+      <div>
+        <label
+          for="password"
+          class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+          >Password</label
+        >
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="••••••••"
+          class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+          required="" />
+      </div>
+      <div>
+        <label
+          for="confirm-password"
+          class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+          >Confirm password</label
+        >
+        <input
+          type="confirm-password"
+          name="confirm-password"
+          id="confirm-password"
+          placeholder="••••••••"
+          class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+          required="" />
+      </div>
+      <div class="flex items-start">
+        <div class="flex h-5 items-center">
+          <input
+            id="terms"
+            aria-describedby="terms"
+            type="checkbox"
+            class="focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 h-4 w-4 rounded border border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800"
+            required="" />
+        </div>
+        <div class="ml-3 text-sm">
+          <label
+            for="terms"
+            class="font-light text-gray-500 dark:text-gray-300"
+            >I accept the
+            <a
+              class="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+              href="#"
+              >Terms and Conditions</a
+            ></label
+          >
+        </div>
+      </div>
+      <button
+        type="submit"
+        class="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4">
+        Create an account
+      </button>
+      <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+        Already have an account?
+        <a
+          href="#"
+          class="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+          >Login here</a
+        >
+      </p>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <div class="h-screen max-w-full bg-login-image bg-cover bg-fixed bg-center bg-no-repeat">
+    <div class="bg-login-image h-screen max-w-full bg-cover bg-fixed bg-center bg-no-repeat">
       <div class="h-screen max-w-full bg-black/60">
         <div class="flex h-screen w-full flex-col items-center justify-center px-6 py-8 lg:py-0">
           <a class="flex items-center">
@@ -9,11 +9,15 @@
               class="mr-3 h-20"
               alt="Pokepedia Logo" />
           </a>
-          <div class="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0"></div>
+          <div class="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
+            <SignUpForm />
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import SignUpForm from '@/components/Forms/SignUpForm.vue';
+</script>
