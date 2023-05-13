90dc8eaf210011e744e662a02842f23cac01b7c1
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-13 15:45:33 +0200

    feat: add ScrollToTopButton

---
+++ src/components/ScrollToTopButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    v-if="y > 1000"
+    @click="scrollToTop"
+    type="button"
+    class="sticky bottom-10 left-10 ml-10 mb-10 cursor-pointer rounded-full bg-red-600 p-4 text-sm font-medium text-white">
+    <Icon
+      icon="material-symbols:arrow-upward"
+      width="26"
+      height="26" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useWindowScroll } from '@vueuse/core';
+import { Icon } from '@iconify/vue';
+
+const { y } = useWindowScroll();
+
+const scrollToTop = (): void => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <Navbar />
-  <About />
-  <Pokedex />
-  <Contact />
-  <Faq />
+  <main class="relative bg-background-light dark:bg-background-dark">
+    <About />
+    <Pokedex />
+    <Contact />
+    <Faq />
+    <ScrollToTopButton />
+  </main>
   <Footer />
 </template>
 
@@ -13,5 +16,6 @@ import About from '@/components/About.vue';
 import Pokedex from '@/components/Pokedex.vue';
 import Contact from '@/components/Contact.vue';
 import Faq from '@/components/Faq.vue';
+import ScrollToTopButton from '@/components/ScrollToTopButton.vue';
 import Footer from '@/components/Footer.vue';
 </script>
