906eed675371e63a465b5f982c9834eff71f6d91
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-14 00:11:38 +0200

    style: add styling changes in the Sign up form component

--- src/components/Forms/SignUpForm.vue
+++ src/components/Forms/SignUpForm.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
-    <h1
-      class="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
+    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
       Create and account
     </h1>
     <form
@@ -10,21 +9,21 @@
       <div>
         <label
           for="email"
-          class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+          class="mb-2 block text-sm font-medium text-gray-900"
           >Your email</label
         >
         <input
           type="email"
           name="email"
           id="email"
-          class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
-          placeholder="name@company.com"
-          required="" />
+          class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
+          placeholder="mail@example.com"
+          required="true" />
       </div>
       <div>
         <label
           for="password"
-          class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+          class="mb-2 block text-sm font-medium text-gray-900"
           >Password</label
         >
         <input
@@ -32,13 +31,13 @@
           name="password"
           id="password"
           placeholder="••••••••"
-          class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
-          required="" />
+          class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
+          required="true" />
       </div>
       <div>
         <label
           for="confirm-password"
-          class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+          class="mb-2 block text-sm font-medium text-gray-900"
           >Confirm password</label
         >
         <input
@@ -46,8 +45,8 @@
           name="confirm-password"
           id="confirm-password"
           placeholder="••••••••"
-          class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
-          required="" />
+          class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
+          required="true" />
       </div>
       <div class="flex items-start">
         <div class="flex h-5 items-center">
@@ -55,16 +54,16 @@
             id="terms"
             aria-describedby="terms"
             type="checkbox"
-            class="focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 h-4 w-4 rounded border border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800"
-            required="" />
+            class="h-4 w-4 rounded border border-gray-300 bg-gray-50 accent-red-600"
+            required="true" />
         </div>
         <div class="ml-3 text-sm">
           <label
             for="terms"
-            class="font-light text-gray-500 dark:text-gray-300"
+            class="font-light text-gray-500"
             >I accept the
             <a
-              class="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+              class="font-medium text-red-600 hover:underline"
               href="#"
               >Terms and Conditions</a
             ></label
@@ -73,16 +72,18 @@
       </div>
       <button
         type="submit"
-        class="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4">
+        class="w-full rounded-lg bg-red-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-700">
         Create an account
       </button>
-      <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+      <p class="text-sm font-light text-gray-500">
         Already have an account?
-        <a
-          href="#"
-          class="text-primary-600 dark:text-primary-500 font-medium hover:underline"
-          >Login here</a
-        >
+        <router-link to="/signin">
+          <a
+            href="#"
+            class="font-medium text-red-600 hover:underline"
+            >Login here</a
+          >
+        </router-link>
       </p>
     </form>
   </div>
