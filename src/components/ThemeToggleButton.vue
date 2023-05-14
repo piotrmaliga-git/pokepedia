3843c0c19dc5a03f22144915e1aef2c8044984a7
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-14 20:26:04 +0200

    ref: change classes naming in theme button

--- src/components/ThemeToggleButton.vue
+++ src/components/ThemeToggleButton.vue
@@ -2,7 +2,7 @@
   <button
     @click="toggleDark()"
     class="mr-2 rounded-lg p-2"
-    :class="isDark ? 'theme-toggle-btn-light' : 'theme-toggle-btn-dark'">
+    :class="isDark ? 'theme-btn' : 'theme-btn--dark'">
     <Icon
       :icon="isDark ? 'bi:sun' : 'ph:moon-fill'"
       width="21"
