bd5acc4497cd5d11ba2e4ad77c13f073550afdc7
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-14 19:37:58 +0200

    ref: add custom styling classes for navbar links

--- src/components/NavbarLinks.vue
+++ src/components/NavbarLinks.vue
@@ -6,51 +6,43 @@
       class="mt-4 flex flex-col rounded-lg bg-gray-100 p-4 font-medium dark:bg-gray-700 lg:mt-0 lg:flex-row lg:space-x-8 lg:border-0 lg:bg-transparent lg:p-0 dark:lg:bg-transparent">
       <li>
         <a
-          href="#"
-          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
-          aria-current="page"
-          >Home</a
-        >
-      </li>
-      <li>
-        <a
           href="#about"
-          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
+          class="navbar-link navbar-link--dark"
           >About</a
         >
       </li>
       <li>
         <a
           href="#pokedex"
-          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
+          class="navbar-link navbar-link--dark"
           >Pokédex</a
         >
       </li>
       <li>
         <a
           href="#"
-          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
+          class="navbar-link navbar-link--dark"
           >Games & Apps</a
         >
       </li>
       <li>
         <a
           href="#"
-          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
+          class="navbar-link navbar-link--dark"
           >Trading Card Game</a
         >
       </li>
       <li>
         <a
           href="#contact"
-          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
+          class="navbar-link navbar-link--dark"
           >Contact</a
         >
       </li>
       <li>
         <a
           href="#faq"
-          class="block rounded py-2 pl-3 pr-48 text-gray-700 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white lg:p-0 lg:text-white lg:hover:scale-105 lg:hover:bg-transparent lg:hover:text-red-600 dark:lg:text-white dark:lg:hover:bg-transparent dark:lg:hover:text-red-600"
+          class="navbar-link navbar-link--dark"
           >FAQ</a
         >
       </li>
