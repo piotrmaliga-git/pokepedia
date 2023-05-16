2c82d45ef9669522f4ca6036b319d5c0ea4316d0
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-16 20:57:13 +0200

    ref: move ScrollToTopButton component in to the separate folder

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,6 +16,6 @@ import About from '@/components/About/About.vue';
 import Pokedex from '@/components/Pokedex/Pokedex.vue';
 import Contact from '@/components/Contact/Contact.vue';
 import Faq from '@/components/Faq/Faq.vue';
-import ScrollToTopButton from '@/components/Buttons/ScrollToTopButton.vue';
+import ScrollToTopButton from '@/components/ScrollToTopButton/ScrollToTopButton.vue';
 import Footer from '@/components/Footer/Footer.vue';
 </script>
