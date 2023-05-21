f5b7ac6ef229d311d89dc5fa4cc0af03cfddaef2
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-21 14:16:19 +0200

    conf: install vite-plugin-vue-type-imports + add recommended configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, type PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { visualizer } from 'rollup-plugin-visualizer';
+import VueTypeImports from 'vite-plugin-vue-type-imports';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), visualizer() as PluginOption],
+  plugins: [vue(), visualizer() as PluginOption, VueTypeImports()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
