6abeb2475546908ccc8c77b0c6ffc1dc10bca8c9
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-22 19:11:46 +0200

    feat: migrate About modal data text to the constants data directory

---
+++ src/constants/modals.ts
@@ -0,0 +1,6 @@
+export const AboutModal = {
+  id: 'about modal',
+  tabindex: '-1',
+  headingTitle: 'About',
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatum illo porro non quos. Quidem, excepturi molestias? Voluptate recusandae incidunt velit. Necessitatibus sit, quos nesciunt nemo delectus consequatur. Neque, perferendis.',
+};
