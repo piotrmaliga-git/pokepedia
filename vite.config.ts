e9dec0d50cb401a4ca065245e8b6c553d41e28cb
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-22 19:16:37 +0200

    fix: uninstall vite-plugin-vue-type-imports package from the project

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, type PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { visualizer } from 'rollup-plugin-visualizer';
-import VueTypeImports from 'vite-plugin-vue-type-imports';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), visualizer() as PluginOption, VueTypeImports()],
+  plugins: [vue(), visualizer() as PluginOption],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
