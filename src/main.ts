f3ee7b56454efa94b3fde72d2ada57dc897f4cde
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-23 21:22:25 +0200

    conf: add i18n library into the main.ts + add directly chain of createApp and app.use and delete createPinia import

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+
+import i18n from './i18n';
 
 import './assets/styles/main.css';
 
 import '@fontsource/rubik';
 import '@fontsource/rubik/700.css';
 
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount('#app');
+createApp(App).use(router).use(i18n).mount('#app')
