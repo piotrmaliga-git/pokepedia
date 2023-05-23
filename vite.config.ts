f3b554d23909a2ffe68d68dd1362f61ad62511b1
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-23 21:17:48 +0200

    conf: install vue-i18n and @intlify/unplugin-vue-i18n packages + configuration in vite.conf.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { fileURLToPath, URL } from 'node:url';
+import { resolve, dirname } from 'node:path';
 
 import { defineConfig, type PluginOption } from 'vite';
+
 import vue from '@vitejs/plugin-vue';
 import { visualizer } from 'rollup-plugin-visualizer';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), visualizer() as PluginOption],
+  plugins: [
+    vue(),
+    visualizer() as PluginOption,
+    VueI18nPlugin({
+      runtimeOnly: false,
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**'),
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
