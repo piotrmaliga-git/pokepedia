2ca406761854b6ae10f0fe9e61f70c3b6ce2f1fe
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-24 23:03:54 +0200

    fix: delete header.ts from constants directory

--- src/constants/header.ts
+++
@@ -1,8 +0,0 @@
-export const NavbarLinks = {
-  about: 'About',
-  pokedex: 'Pokédex',
-  gamesAndApps: 'Games & Apps',
-  tradingCardGame: 'Trading Card Game',
-  contact: 'Contact',
-  faq: 'FAQ',
-};
