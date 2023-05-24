32a17b9b89311c7858f7b28ad4eb2cffe5e9fca2
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-24 23:02:53 +0200

    feat: add en and pl.json + import them to the i18n index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,8 +1,12 @@
 import { createI18n } from 'vue-i18n';
 
+import en from './locales/english/en.json';
+import pl from './locales/polish/pl.json';
+
 export default createI18n({
   locale: import.meta.env.VITE_DEFAULT_LOCALE,
   fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
   legacy: false,
   globalInjection: true,
+  messages: { en, pl },
 });
