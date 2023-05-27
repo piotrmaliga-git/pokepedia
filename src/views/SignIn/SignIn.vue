d51699f9aa3247698ae77470f31ee4ba6dada4c1
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-05-27 18:01:34 +0200

    style: replace link tag to div on the page logo in signing views + increase logo height

--- src/views/SignIn/SignIn.vue
+++ src/views/SignIn/SignIn.vue
@@ -3,12 +3,12 @@
     <div class="h-screen max-w-full bg-auth bg-cover bg-fixed bg-center bg-no-repeat">
       <div class="h-screen max-w-full bg-black/60">
         <div class="flex h-screen w-full flex-col items-center justify-center px-6 py-8 lg:py-0">
-          <a class="flex items-center">
+          <div class="flex items-center">
             <Image
-              class="mr-3 h-20"
+              class="mr-3 h-24"
               alt="Pokepedia Logo"
               src="src/assets/images/logo/Pokepedia.png" />
-          </a>
+          </div>
           <div class="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
             <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
