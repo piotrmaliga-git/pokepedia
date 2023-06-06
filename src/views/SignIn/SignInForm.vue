c1c2d204a483e93f166937b0e7edffd4dba855f3
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-06-06 21:52:10 +0200

    feat: implement sign in buttons in with Facebook and Google in the SignInForm component

--- src/views/SignIn/SignInForm.vue
+++ src/views/SignIn/SignInForm.vue
@@ -30,6 +30,35 @@
         class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
         required="true" />
     </div>
+    <div class="inline-flex w-full items-center justify-center">
+      <hr class="my-4 h-px w-full border-0 bg-gray-200 dark:bg-gray-700" />
+      <span
+        class="absolute left-1/2 -translate-x-1/2 bg-white px-3 font-medium text-gray-900 dark:bg-gray-900 dark:text-white"
+        >or</span
+      >
+    </div>
+    <div class="flex items-center justify-between">
+      <button
+        type="button"
+        class="mb-2 inline-flex w-full items-center rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        <Icon
+          class="-ml-1 mr-2"
+          icon="logos:facebook"
+          width="20"
+          height="20" />
+        Sign in with Facebook
+      </button>
+      <button
+        type="button"
+        class="mb-2 ml-4 inline-flex w-full items-center rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        <Icon
+          class="-ml-1 mr-2"
+          icon="logos:google-icon"
+          width="20"
+          height="20" />
+        Sign in with Google
+      </button>
+    </div>
     <div class="flex items-center justify-between">
       <div class="flex items-start">
         <div class="flex h-5 items-center">
@@ -69,3 +98,7 @@
     </p>
   </form>
 </template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+</script>
