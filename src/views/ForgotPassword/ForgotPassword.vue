f096eb6bde7cb880a2da77366b165ea7596fa089
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-06-06 21:45:11 +0200

    feat: implement ForgotPassword view + add ForgotPasswordForm component with basic template from Flowbite

---
+++ src/views/ForgotPassword/ForgotPassword.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <div class="h-screen max-w-full bg-auth bg-cover bg-fixed bg-center bg-no-repeat">
+      <div class="h-screen max-w-full bg-black/60">
+        <div class="flex h-screen w-full flex-col items-center justify-center px-6 py-8 lg:py-0">
+          <div class="flex items-center">
+            <Image
+              class="mr-3 h-24"
+              alt="Pokepedia Logo"
+              src="src/assets/images/logo/Pokepedia.png" />
+          </div>
+          <div class="w-full rounded-lg bg-white shadow sm:max-w-lg md:mt-0 xl:p-0">
+            <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
+              <h2
+                class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
+                Change Password
+              </h2>
+              <ForgotPasswordForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import ForgotPasswordForm from './ForgotPasswordForm.vue';
+import Image from '@shared/Image/Image.vue';
+</script>
---
+++ src/views/ForgotPassword/ForgotPasswordForm.vue
@@ -0,0 +1,84 @@
+<template>
+  <form
+    class="mt-4 space-y-4 md:space-y-5 lg:mt-5"
+    action="#">
+    <div>
+      <label
+        for="email"
+        class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+        >Your email</label
+      >
+      <input
+        type="email"
+        name="email"
+        id="email"
+        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
+        placeholder="name@company.com"
+        required="true" />
+    </div>
+    <div>
+      <label
+        for="password"
+        class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+        >New Password</label
+      >
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="••••••••"
+        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
+        required="true" />
+    </div>
+    <div>
+      <label
+        for="confirm-password"
+        class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+        >Confirm password</label
+      >
+      <input
+        type="confirm-password"
+        name="confirm-password"
+        id="confirm-password"
+        placeholder="••••••••"
+        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
+        required="true" />
+    </div>
+    <div class="flex items-start">
+      <div class="flex h-5 items-center">
+        <input
+          id="newsletter"
+          aria-describedby="newsletter"
+          type="checkbox"
+          class="h-4 w-4 rounded border border-gray-300 bg-gray-50 accent-red-600"
+          required="true" />
+      </div>
+      <div class="ml-3 text-sm">
+        <label
+          for="newsletter"
+          class="font-light text-gray-500"
+          >I accept the
+          <a
+            class="font-medium text-red-600 hover:underline"
+            href="#"
+            >Terms and Conditions</a
+          ></label
+        >
+      </div>
+    </div>
+    <button
+      type="submit"
+      class="w-full rounded-lg bg-red-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-700">
+      Reset password
+    </button>
+    <p class="flex justify-end text-sm font-light text-gray-500">
+      <router-link
+        to="/signin"
+        class="font-medium text-red-600 hover:underline"
+        >Back to Login</router-link
+      >
+    </p>
+  </form>
+</template>
+
+<script setup lang="ts"></script>
