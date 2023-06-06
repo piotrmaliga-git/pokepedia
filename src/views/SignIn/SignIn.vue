74d7395f2565eb4985f95afe82821232bbfd97b8
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-06-06 22:26:35 +0200

    feat: implement AuthWrapper shared component into the SignIn view

--- src/views/SignIn/SignIn.vue
+++ src/views/SignIn/SignIn.vue
@@ -1,29 +1,11 @@
 <template>
-  <section>
-    <div class="h-screen max-w-full bg-auth bg-cover bg-fixed bg-center bg-no-repeat">
-      <div class="h-screen max-w-full bg-black/60">
-        <div class="flex h-screen w-full flex-col items-center justify-center px-6 py-8 lg:py-0">
-          <div class="flex items-center">
-            <Image
-              class="mr-3 h-24"
-              alt="Pokepedia Logo"
-              src="src/assets/images/logo/Pokepedia.png" />
-          </div>
-          <div class="w-full rounded-lg bg-white shadow sm:max-w-lg md:mt-0 xl:p-0">
-            <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                Sign in to your account
-              </h1>
-              <SignInForm />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <AuthWrapper>
+    Sign in to your account
+    <SignInForm />
+  </AuthWrapper>
 </template>
 
 <script setup lang="ts">
-import SignInForm from './SignInForm.vue';
-import Image from '@shared/Image/Image.vue';
+import SignInForm from '@views/SignIn/SignInForm.vue'
+import AuthWrapper from '@shared/AuthWrapper/AuthWrapper.vue'
 </script>
