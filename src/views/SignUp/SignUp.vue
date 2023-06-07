f6cc103ed67a8bb44095b52168390dac829637c7
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-06-07 20:29:29 +0200

    feat: use AuthWrapper shared component in SignUp view

--- src/views/SignUp/SignUp.vue
+++ src/views/SignUp/SignUp.vue
@@ -1,29 +1,22 @@
 <template>
-  <section>
-    <div class="h-screen max-w-full bg-auth bg-cover bg-fixed bg-center bg-no-repeat">
-      <div class="h-screen max-w-full bg-black/60">
-        <div class="flex h-screen w-full flex-col items-center justify-center px-6 py-8 lg:py-0">
-          <div class="flex items-center">
-            <Image
-              class="mr-3 h-24"
-              alt="Pokepedia Logo"
-              src="src/assets/images/logo/Pokepedia.png" />
-          </div>
-          <div class="w-full rounded-lg bg-white shadow sm:max-w-lg md:mt-0 xl:p-0">
-            <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                Create and account
-              </h1>
-              <SignUpForm />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <AuthWrapper>
+    <template #authLogoImage>
+      <Image
+        class="mr-3 h-24"
+        alt="Pokepedia Logo"
+        :src="logoImage" />
+    </template>
+    <template #authFormTitle>Create and account</template>
+    <template #authFormBody>
+      <SignUpForm />
+    </template>
+  </AuthWrapper>
 </template>
 
 <script setup lang="ts">
-import SignUpForm from './SignUpForm.vue';
+import AuthWrapper from '@shared/AuthWrapper/AuthWrapper.vue';
+import SignUpForm from '@views/SignUp/SignUpForm.vue';
+
 import Image from '@shared/Image/Image.vue';
+import logoImage from '@assets/images/logo/Pokepedia.png';
 </script>
