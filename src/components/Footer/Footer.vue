b0ff76512f067cb940c54df2d83f079d0ec4b2da
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-06-08 13:06:05 +0200

    ref: split Footer Component into smaller pieces. Add FooterLinks Component

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -12,65 +12,7 @@
               :src="logoImage" />
           </router-link>
         </div>
-        <div class="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
-          <div>
-            <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900">Resources</h2>
-            <ul class="font-medium text-white">
-              <li class="mb-4">
-                <a
-                  href="https://flowbite.com/"
-                  class="hover:underline"
-                  >Flowbite</a
-                >
-              </li>
-              <li>
-                <a
-                  href="https://tailwindcss.com/"
-                  class="hover:underline"
-                  >Tailwind CSS</a
-                >
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900">Follow us</h2>
-            <ul class="font-medium text-white">
-              <li class="mb-4">
-                <a
-                  href="https://github.com/themesberg/flowbite"
-                  class="hover:underline"
-                  >Github</a
-                >
-              </li>
-              <li>
-                <a
-                  href="https://discord.gg/4eeurUVvTy"
-                  class="hover:underline"
-                  >Discord</a
-                >
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900">Legal</h2>
-            <ul class="font-medium text-white">
-              <li class="mb-4">
-                <a
-                  href="#"
-                  class="hover:underline"
-                  >Privacy Policy</a
-                >
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="hover:underline"
-                  >Terms &amp; Conditions</a
-                >
-              </li>
-            </ul>
-          </div>
-        </div>
+        <FooterLinks/>
       </div>
       <hr class="my-6 border-l-gray-200 sm:mx-auto lg:my-8" />
       <div class="sm:flex sm:items-center sm:justify-between">
@@ -140,6 +82,7 @@
 </template>
 
 <script setup lang="ts">
+import FooterLinks from "./FooterLinks.vue"
 import Image from '@shared/Image/Image.vue';
 import Modal from '@shared/Modal/Modal.vue';
 import { Icon } from '@iconify/vue';
---
+++ src/components/Footer/FooterLinks.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
+    <div>
+      <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900">Resources</h2>
+      <ul class="font-medium text-white">
+        <li class="mb-4">
+          <a
+            href="https://flowbite.com/"
+            class="hover:underline"
+            >Flowbite</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://tailwindcss.com/"
+            class="hover:underline"
+            >Tailwind CSS</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900">Follow us</h2>
+      <ul class="font-medium text-white">
+        <li class="mb-4">
+          <a
+            href="https://github.com/themesberg/flowbite"
+            class="hover:underline"
+            >Github</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://discord.gg/4eeurUVvTy"
+            class="hover:underline"
+            >Discord</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900">Legal</h2>
+      <ul class="font-medium text-white">
+        <li class="mb-4">
+          <a
+            href="#"
+            class="hover:underline"
+            >Privacy Policy</a
+          >
+        </li>
+        <li>
+          <a
+            href="#"
+            class="hover:underline"
+            >Terms &amp; Conditions</a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
