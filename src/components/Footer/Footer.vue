731509b335f63647aa1e62b6c39e4da667efcf0f
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-06-08 12:56:53 +0200

    feat: implementing more extensive Footer template from Flowbite

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,83 +1,140 @@
 <template>
   <footer class="bg-red-600">
-    <div class="mx-auto w-full max-w-screen-xl p-4 md:py-8">
-      <div class="sm:flex sm:items-center sm:justify-between">
-        <router-link
-          to="/"
-          class="mb-4 flex items-center sm:mb-0">
-          <Image
-            class="mr-3 h-12"
-            alt="Pokepedia Logo"
-            :src="logoImage" />
-        </router-link>
-        <ul class="mb-6 flex flex-wrap items-center text-sm font-medium text-white sm:mb-0">
-          <li>
-            <a
-              class="mr-4 cursor-pointer hover:underline md:mr-6"
-              data-modal-target="about-modal"
-              data-modal-toggle="about-modal"
-              >About</a
-            >
-          </li>
-          <Modal
-            id="about-modal"
-            dataModalHide="about-modal"
-            tabindex="-1"
-            headingTitle="About"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatum illo porro non quos. Quidem, excepturi molestias? Voluptate recusandae incidunt velit. Necessitatibus sit, quos nesciunt nemo delectus consequatur. Neque, perferendis." />
-          <li>
-            <a
-              class="mr-4 cursor-pointer hover:underline md:mr-6"
-              data-modal-target="policy-modal"
-              data-modal-toggle="policy-modal"
-              >Privacy Policy</a
-            >
-          </li>
-          <Modal
-            id="policy-modal"
-            dataModalHide="policy-modal"
-            tabindex="-1"
-            headingTitle="Privacy Policy"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatum illo porro non quos. Quidem, excepturi molestias? Voluptate recusandae incidunt velit. Necessitatibus sit, quos nesciunt nemo delectus consequatur. Neque, perferendis." />
-          <li>
-            <a
-              class="mr-4 cursor-pointer hover:underline md:mr-6"
-              data-modal-target="license-modal"
-              data-modal-toggle="license-modal"
-              >Licensing</a
-            >
-          </li>
-          <Modal
-            id="license-modal"
-            dataModalHide="license-modal"
-            tabindex="-1"
-            headingTitle="Licensing"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatum illo porro non quos. Quidem, excepturi molestias? Voluptate recusandae incidunt velit. Necessitatibus sit, quos nesciunt nemo delectus consequatur. Neque, perferendis." />
-          <li>
-            <a
-              class="cursor-pointer hover:underline"
-              data-modal-target="contact-modal"
-              data-modal-toggle="contact-modal"
-              >Contact</a
-            >
-          </li>
-          <Modal
-            id="contact-modal"
-            dataModalHide="contact-modal"
-            tabindex="-1"
-            headingTitle="Contact"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatum illo porro non quos. Quidem, excepturi molestias? Voluptate recusandae incidunt velit. Necessitatibus sit, quos nesciunt nemo delectus consequatur. Neque, perferendis." />
-        </ul>
+    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+      <div class="md:flex md:justify-between">
+        <div class="mb-6 md:mb-0">
+          <router-link
+            to="/"
+            class="flex items-center">
+            <Image
+              class="mr-3 h-12"
+              alt="Pokepedia Logo"
+              :src="logoImage" />
+          </router-link>
+        </div>
+        <div class="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
+          <div>
+            <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900">Resources</h2>
+            <ul class="font-medium text-white">
+              <li class="mb-4">
+                <a
+                  href="https://flowbite.com/"
+                  class="hover:underline"
+                  >Flowbite</a
+                >
+              </li>
+              <li>
+                <a
+                  href="https://tailwindcss.com/"
+                  class="hover:underline"
+                  >Tailwind CSS</a
+                >
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900">Follow us</h2>
+            <ul class="font-medium text-white">
+              <li class="mb-4">
+                <a
+                  href="https://github.com/themesberg/flowbite"
+                  class="hover:underline"
+                  >Github</a
+                >
+              </li>
+              <li>
+                <a
+                  href="https://discord.gg/4eeurUVvTy"
+                  class="hover:underline"
+                  >Discord</a
+                >
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900">Legal</h2>
+            <ul class="font-medium text-white">
+              <li class="mb-4">
+                <a
+                  href="#"
+                  class="hover:underline"
+                  >Privacy Policy</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="hover:underline"
+                  >Terms &amp; Conditions</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
       <hr class="my-6 border-l-gray-200 sm:mx-auto lg:my-8" />
-      <span class="block text-sm text-white sm:text-center"
-        >&copy; {{ currentYear }}
-        <router-link
-          to="/"
-          class="hover:underline"
-          >Pokepedia&trade;</router-link
-        >. All Rights Reserved.</span
-      >
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <span class="text-sm text-white sm:text-center"
+          >&copy; {{ currentYear }}
+          <router-link
+            to="/"
+            class="hover:underline"
+            >Pokepedia&trade;</router-link
+          >. All Rights Reserved.
+        </span>
+        <div class="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            class="text-white hover:text-gray-200">
+            <Icon
+              icon="mdi:facebook"
+              width="20"
+              height="20" />
+            <span class="sr-only">Facebook page</span>
+          </a>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            class="text-white hover:text-gray-200">
+            <Icon
+              icon="mdi:instagram"
+              width="20"
+              height="20" />
+            <span class="sr-only">Instagram page</span>
+          </a>
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            class="text-white hover:text-gray-200">
+            <Icon
+              icon="mdi:twitter"
+              width="20"
+              height="20" />
+            <span class="sr-only">Twitter page</span>
+          </a>
+          <a
+            href="https://github.com/piotrmaliga-git/pokepedia"
+            target="_blank"
+            class="text-white hover:text-gray-200">
+            <Icon
+              icon="mdi:github"
+              width="20"
+              height="20" />
+            <span class="sr-only">GitHub account</span>
+          </a>
+          <a
+            href="https://dribbble.com"
+            target="_blank"
+            class="text-white hover:text-gray-200">
+            <Icon
+              icon="ph:dribbble-logo"
+              width="20"
+              height="20" />
+            <span class="sr-only">Dribbble account</span>
+          </a>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
@@ -85,12 +142,13 @@
 <script setup lang="ts">
 import Image from '@shared/Image/Image.vue';
 import Modal from '@shared/Modal/Modal.vue';
+import { Icon } from '@iconify/vue';
 
 import { onMounted } from 'vue';
 import { initFlowbite } from 'flowbite';
 import { useNow, useDateFormat } from '@vueuse/core';
 
-import logoImage from '@assets/images/logo/Pokepedia.png'
+import logoImage from '@assets/images/logo/Pokepedia.png';
 
 onMounted(() => {
   initFlowbite();
