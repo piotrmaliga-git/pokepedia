a44930b6039db9948373d66855a979496a76eb64
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-06-08 13:14:56 +0200

    ref: split Footer Component into smaller pieces. Add FooterIcons Component

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -12,7 +12,7 @@
               :src="logoImage" />
           </router-link>
         </div>
-        <FooterLinks/>
+        <FooterLinks />
       </div>
       <hr class="my-6 border-l-gray-200 sm:mx-auto lg:my-8" />
       <div class="sm:flex sm:items-center sm:justify-between">
@@ -24,74 +24,22 @@
             >Pokepedia&trade;</router-link
           >. All Rights Reserved.
         </span>
-        <div class="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-          <a
-            href="https://www.facebook.com"
-            target="_blank"
-            class="text-white hover:text-gray-200">
-            <Icon
-              icon="mdi:facebook"
-              width="20"
-              height="20" />
-            <span class="sr-only">Facebook page</span>
-          </a>
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            class="text-white hover:text-gray-200">
-            <Icon
-              icon="mdi:instagram"
-              width="20"
-              height="20" />
-            <span class="sr-only">Instagram page</span>
-          </a>
-          <a
-            href="https://twitter.com"
-            target="_blank"
-            class="text-white hover:text-gray-200">
-            <Icon
-              icon="mdi:twitter"
-              width="20"
-              height="20" />
-            <span class="sr-only">Twitter page</span>
-          </a>
-          <a
-            href="https://github.com/piotrmaliga-git/pokepedia"
-            target="_blank"
-            class="text-white hover:text-gray-200">
-            <Icon
-              icon="mdi:github"
-              width="20"
-              height="20" />
-            <span class="sr-only">GitHub account</span>
-          </a>
-          <a
-            href="https://dribbble.com"
-            target="_blank"
-            class="text-white hover:text-gray-200">
-            <Icon
-              icon="ph:dribbble-logo"
-              width="20"
-              height="20" />
-            <span class="sr-only">Dribbble account</span>
-          </a>
-        </div>
+        <FooterIcons />
       </div>
     </div>
   </footer>
 </template>
 
 <script setup lang="ts">
-import FooterLinks from "./FooterLinks.vue"
+import FooterLinks from '@components/Footer/FooterLinks.vue';
+import FooterIcons from '@components/Footer/FooterIcons.vue';
+
 import Image from '@shared/Image/Image.vue';
-import Modal from '@shared/Modal/Modal.vue';
-import { Icon } from '@iconify/vue';
+import logoImage from '@assets/images/logo/Pokepedia.png';
 
 import { onMounted } from 'vue';
 import { initFlowbite } from 'flowbite';
 import { useNow, useDateFormat } from '@vueuse/core';
-
-import logoImage from '@assets/images/logo/Pokepedia.png';
 
 onMounted(() => {
   initFlowbite();
---
+++ src/components/Footer/FooterIcons.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+    <a
+      href="https://www.facebook.com"
+      target="_blank"
+      class="text-white hover:text-gray-200">
+      <Icon
+        icon="mdi:facebook"
+        width="20"
+        height="20" />
+      <span class="sr-only">Facebook page</span>
+    </a>
+    <a
+      href="https://www.instagram.com"
+      target="_blank"
+      class="text-white hover:text-gray-200">
+      <Icon
+        icon="mdi:instagram"
+        width="20"
+        height="20" />
+      <span class="sr-only">Instagram page</span>
+    </a>
+    <a
+      href="https://twitter.com"
+      target="_blank"
+      class="text-white hover:text-gray-200">
+      <Icon
+        icon="mdi:twitter"
+        width="20"
+        height="20" />
+      <span class="sr-only">Twitter page</span>
+    </a>
+    <a
+      href="https://github.com/piotrmaliga-git/pokepedia"
+      target="_blank"
+      class="text-white hover:text-gray-200">
+      <Icon
+        icon="mdi:github"
+        width="20"
+        height="20" />
+      <span class="sr-only">GitHub account</span>
+    </a>
+    <a
+      href="https://dribbble.com"
+      target="_blank"
+      class="text-white hover:text-gray-200">
+      <Icon
+        icon="ph:dribbble-logo"
+        width="20"
+        height="20" />
+      <span class="sr-only">Dribbble account</span>
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+</script>
