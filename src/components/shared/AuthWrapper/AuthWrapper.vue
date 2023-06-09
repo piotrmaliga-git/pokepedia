2085ebd6335ff1aabb79604ae36f5ac85aa4ad1a
Piotr Maliga <piotr.maliga15813@gmail.com>
2023-06-09 18:16:01 +0200

    style: add bg-black/60 alpha cover to the section tag on the auth view

--- src/components/shared/AuthWrapper/AuthWrapper.vue
+++ src/components/shared/AuthWrapper/AuthWrapper.vue
@@ -1,19 +1,16 @@
 <template>
-  <section>
-    <div class="h-screen max-w-full bg-auth bg-cover bg-fixed bg-center bg-no-repeat">
-      <div class="h-screen max-w-full bg-black/60">
-        <div class="flex h-screen w-full flex-col items-center justify-center px-6 py-8 lg:py-0">
-          <div class="flex items-center">
-            <slot name="authLogoImage"></slot>
-          </div>
-          <div class="w-full rounded-lg bg-white shadow sm:max-w-lg md:mt-0 xl:p-0">
-            <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                <slot name="authFormTitle"></slot>
-              </h1>
-              <slot name="authFormBody"></slot>
-            </div>
-          </div>
+  <section
+    class="h-screen max-w-full bg-black/60 bg-auth bg-cover bg-fixed bg-center bg-no-repeat bg-blend-multiply">
+    <div class="flex h-screen w-full flex-col items-center justify-center px-6 py-8 lg:py-0">
+      <div class="flex items-center">
+        <slot name="authLogoImage"></slot>
+      </div>
+      <div class="w-full rounded-lg bg-white shadow sm:max-w-lg md:mt-0 xl:p-0">
+        <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
+          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+            <slot name="authFormTitle"></slot>
+          </h1>
+          <slot name="authFormBody"></slot>
         </div>
       </div>
     </div>
