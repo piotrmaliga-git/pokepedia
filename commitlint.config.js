cde3716dd2d544c892104154a436f9b94a1d2da3
Piotr Maliga <piotr.maliga@futurum.tech>
2023-07-01 00:36:28 +0200

    fix: delete commit linting functionality

--- commitlint.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'hotfix', 'ref', 'style', 'conf', 'docs', 'test']],
-  },
-};
