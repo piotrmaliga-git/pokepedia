b975f6b4ae181dc0d6903b3a83c01d44dcab8d7e
piotr.maliga <piotr.maliga.work@gmail.com>
2023-12-24 13:01:12 +0100

    fix: delete commented code and deprecated  frameborder attribute from iframe tag

--- src/components/GamesAndApps/GamesAndApps.vue
+++ src/components/GamesAndApps/GamesAndApps.vue
@@ -1,15 +1,14 @@
 <template>
   <section id="gamesAndApps">
-    <div class="mx-auto max-w-screen-xl items-center gap-16 px-4 py-8 lg:grid lg:grid-cols-2">
+    <div class="items-center max-w-screen-xl gap-16 px-4 py-8 mx-auto lg:grid lg:grid-cols-2">
       <div class="font-light text-gray-500 dark:text-gray-400 sm:text-lg">
         <GamesAndAppsTabs />
       </div>
       <div>
         <iframe
-          class="mx-auto mt-16 h-64 w-full rounded-lg shadow-xl sm:h-96 lg:mt-0 lg:max-w-xl"
+          class="w-full h-64 mx-auto mt-16 rounded-lg shadow-xl sm:h-96 lg:mt-0 lg:max-w-xl"
           src="https://www.youtube.com/embed/SWtDeeXtMZM"
           title="Pokémon GO - Get Up and Go!"
-          frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen></iframe>
       </div>
