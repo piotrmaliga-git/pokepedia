dfbb6b11116d4946e91624ebe2af2964f260c55c
piotr.maliga <piotr.maliga.work@gmail.com>
2023-12-29 20:42:06 +0100

    conf: add new rules in to the eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,4 +15,26 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   plugins: ['tailwindcss'],
+  ignorePatterns: ['*.config.js'],
+  rules: {
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: [
+          'Header',
+          'Navbar',
+          'Hero',
+          'About',
+          'Pokedex',
+          'Image',
+          'Contact',
+          'Faq',
+          'Footer',
+          'Modal',
+          'Toast',
+          'Home',
+        ],
+      },
+    ],
+  },
 };
